d2dcd81e85c9d9cf913bbf99df5198a08fb1dc57
Tuananh <tuananh48218@gmail.com>
2025-04-06 19:33:28 +0700

    Enhance environment configuration by introducing EnvConfig class for dotenv management. Update FirebaseConfig to utilize EnvConfig for retrieving Firebase credentials, and improve Firebase initialization logic. Modify ServerApplication to load dotenv only if not in Railway environment.

--- server/src/main/java/io/foodapp/server/ServerApplication.java
+++ server/src/main/java/io/foodapp/server/ServerApplication.java
@@ -9,10 +9,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class ServerApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> {
-			System.setProperty(entry.getKey(), entry.getValue());
-		});
+		if (System.getenv("RAILWAY_ENVIRONMENT") == null) {
+			Dotenv dotenv = Dotenv.load();
+			dotenv.entries().forEach(entry -> {
+				System.setProperty(entry.getKey(), entry.getValue());
+			});
+		}
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
--- server/src/main/java/io/foodapp/server/configs/FirebaseConfig.java
+++ server/src/main/java/io/foodapp/server/configs/FirebaseConfig.java
@@ -1,27 +1,34 @@
 package io.foodapp.server.configs;
 
-import java.io.FileInputStream;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-
+import java.util.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-
-import io.github.cdimascio.dotenv.Dotenv;
-
 @Configuration
 public class FirebaseConfig {
-    private static final String FIREBASE_PROJECT_ID = Dotenv.load().get("FIREBASE_PROJECT_ID");
+    private static final String FIREBASE_PROJECT_ID = EnvConfig.get("FIREBASE_PROJECT_ID");
+    private static final String FIREBASE_SERVICE_ACCOUNT_BASE64 = EnvConfig.get("FIREBASE_SERVICE_ACCOUNT_BASE64");
+
+
+
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("server/config/serviceAccountKey.json");
+
+        byte[] decodedKey = Base64.getDecoder().decode(FIREBASE_SERVICE_ACCOUNT_BASE64);
+        ByteArrayInputStream serviceAccount = new ByteArrayInputStream(decodedKey);
         FirebaseOptions options = FirebaseOptions.builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .setProjectId(FIREBASE_PROJECT_ID)
                 .build();
-        return FirebaseApp.initializeApp(options);
+        if (FirebaseApp.getApps().isEmpty()) {
+            return FirebaseApp.initializeApp(options);
+        } else {
+            return FirebaseApp.getInstance();
+        }
     }
 }
