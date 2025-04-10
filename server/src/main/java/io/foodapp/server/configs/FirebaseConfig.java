bdcc4fed867028111fc5785d958e2758b629a3fc
Tuananh <tuananh48218@gmail.com>
2025-04-10 20:39:52 +0700

    testdeploy

--- server/src/main/java/io/foodapp/server/configs/FirebaseConfig.java
+++ server/src/main/java/io/foodapp/server/configs/FirebaseConfig.java
@@ -3,18 +3,27 @@ package io.foodapp.server.configs;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Base64;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+
 @Configuration
 public class FirebaseConfig {
-    private static final String FIREBASE_PROJECT_ID = EnvConfig.get("FIREBASE_PROJECT_ID");
-    private static final String FIREBASE_SERVICE_ACCOUNT_BASE64 = EnvConfig.get("FIREBASE_SERVICE_ACCOUNT_BASE64");
+    // private static final String FIREBASE_PROJECT_ID =
+    // EnvConfig.get("FIREBASE_PROJECT_ID");
+    // private static final String FIREBASE_SERVICE_ACCOUNT_BASE64 =
+    // EnvConfig.get("FIREBASE_SERVICE_ACCOUNT_BASE64");
 
+    @Value("${FIREBASE_PROJECT_ID}")
+    private String FIREBASE_PROJECT_ID;
 
+    @Value("${FIREBASE_SERVICE_ACCOUNT_BASE64}")
+    private String FIREBASE_SERVICE_ACCOUNT_BASE64;
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
--- server/src/main/java/io/foodapp/server/controllers/AdminController.java
+++ server/src/main/java/io/foodapp/server/controllers/AdminController.java
@@ -6,18 +6,32 @@ import org.springframework.web.bind.annotation.RestController;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-
 
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
 @Slf4j
 public class AdminController {
-    
+
+    @Value("${CLOUDINARY_CLOUD_NAME}")
+    private String cloudName;
+
+    @Value("${CLOUDINARY_API_KEY}")
+    private String apiKey;
+
+    @Value("${CLOUDINARY_API_SECRET}")
+    private String apiSecret;
+
     @GetMapping
     public String sayHello() {
-        return new String("Hello admin!!!");
+        // return new String("Hello admin!!!");
+        return new String(
+                "Hello admin!!!" + "\n" +
+                        "CLOUDINARY_CLOUD_NAME: " + cloudName + "\n" +
+                        "CLOUDINARY_API_KEY: " + apiKey + "\n" +
+                        "CLOUDINARY_API_SECRET: " + apiSecret + "\n");
     }
-    
+
 }
