e59bef17c9fa50b43acbd7df1ee1e87fe3179229
Le Van Huy <huyle842005@gmail.com>
2025-07-01 22:16:15 +0700

    fix: Correct foodImage field name in OrderItemResponse and update mapping in OrderItemMapper

--- server/src/main/java/io/foodapp/server/mappers/Order/OrderItemMapper.java
+++ server/src/main/java/io/foodapp/server/mappers/Order/OrderItemMapper.java
@@ -1,15 +1,14 @@
 package io.foodapp.server.mappers.Order;
 
-import io.foodapp.server.dtos.Order.OrderItemRequest;
-import io.foodapp.server.dtos.Order.OrderItemResponse;
-import io.foodapp.server.models.Order.OrderItem;
+import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 
-import java.util.List;
-
+import io.foodapp.server.dtos.Order.OrderItemRequest;
+import io.foodapp.server.dtos.Order.OrderItemResponse;
+import io.foodapp.server.models.Order.OrderItem;
 import io.foodapp.server.utils.ImageInfo;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = org.mapstruct.ReportingPolicy.IGNORE, unmappedSourcePolicy = org.mapstruct.ReportingPolicy.IGNORE)
@@ -17,7 +16,7 @@ public interface OrderItemMapper {
 
     OrderItem toEntity(OrderItemRequest dto);
 
-    @Mapping(target = "foodImages", source = "foodImages", qualifiedByName = "mapImageInfo")
+    @Mapping(target = "foodImage", source = "foodImages", qualifiedByName = "mapImageInfo")
     @Mapping(target = "foodId", source = "food.id")
     OrderItemResponse toDTO(OrderItem entity);
 
